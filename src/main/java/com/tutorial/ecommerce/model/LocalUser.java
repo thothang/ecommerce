41810ddae81d0c97000fc135fe021280007894e9
thothang <tn28112017@gmail.com>
2023-12-05 22:15:33 +0700

    ...

--- src/main/java/com/tutorial/ecommerce/model/LocalUser.java
+++ src/main/java/com/tutorial/ecommerce/model/LocalUser.java
@@ -38,10 +38,10 @@ public class LocalUser implements UserDetails {
     @Column(name = "email", nullable = false, unique = true, length = 320)
     private String email;
 
-    @Column(name = "first_name", nullable = false)
+    @Column(name = "firstname", nullable = false)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false)
+    @Column(name = "lastname", nullable = false)
     private String lastName;
 
     @JsonIgnore
